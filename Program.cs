563087a4c291188916366550896ece0cd3087129
Thiago Lanza <lanzathm@gmail.com>
2021-12-25 22:06:44 -0300

    Basic CRUD example (without best practices)

--- Program.cs
+++ Program.cs
@@ -7,10 +7,14 @@ namespace Blog
 {
     class Program
     {
-        private const string CONNECTION_STRING = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$";
+        private const string CONNECTION_STRING = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True";
         static void Main(string[] args)
         {
-            ReadUsers();
+            // ReadUsers();
+            // ReadUser();
+            // CreateUser();
+            // UpdateUser();
+            // DeleteUser();
         }
 
         public static void ReadUsers()
@@ -18,11 +22,67 @@ namespace Blog
             using (var connection = new SqlConnection(CONNECTION_STRING))
             {
                 var users = connection.GetAll<User>();
-
                 foreach (var user in users)
                 {
                     Console.WriteLine($"Id: {user.Id} name: {user.Name} email: {user.Email}");
                 }
+            }
+        }
+
+        public static void ReadUser()
+        {
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var user = connection.Get<User>(1);
+
+                Console.WriteLine(user.Bio);
+            }
+        }
+
+        public static void CreateUser()
+        {
+            var user = new User()
+            {
+                Name = "Jhonny Sins",
+                Bio = "O Careca da Brazzers | Pau para toda obra",
+                Email = "sins@brazzers.com",
+                Password = "$2y$10$XxReFmCyYHKwkJyZGSsDtuRqj2hgYt56pPCVIGSv6xgVJTiTivs8G",
+                Image = "https://i1.sndcdn.com/artworks-000562525653-v1wh6h-t500x500.jpg",
+                Slug = "careca-da-brazzers"
+            };
+
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                connection.Insert<User>(user); // Retorna quantidade de linhas afetadas
+            }
+        }
+
+        public static void UpdateUser()
+        {
+            var user = new User()
+            {
+                Id = 3,
+                Name = "Jhonny Sins",
+                Bio = "O Careca da Brazzers | Pau para toda obra",
+                Email = "jhonny.sins@brazzers.com",
+                Password = "$2y$10$XxReFmCyYHKwkJyZGSsDtuRqj2hgYt56pPCVIGSv6xgVJTiTivs8G",
+                Image = "https://i1.sndcdn.com/artworks-000562525653-v1wh6h-t500x500.jpg",
+                Slug = "careca-da-brazzers"
+            };
+
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                connection.Update<User>(user);
+            }
+        }
+
+        public static void DeleteUser()
+        {
+
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var user = connection.Get<User>(2);
+                connection.Delete<User>(user);
             }
         }
     }
